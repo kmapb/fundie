7970d3d92ac90abce3f3175e7065165e5e0eef36
Keith Adams <kma@pebblebed.com>
2025-07-03 16:11:12 -0700

    Check the other transitions for sanity.

--- test/test_stage.cpp
+++ test/test_stage.cpp
@@ -10,3 +10,17 @@ TEST(Stage, evolve) {
     stage.traverse_stage(asset);
     EXPECT_EQ(asset.value(), 12e6);
 }
+
+TEST(Stage, death) {
+    Stage death {"Die", 2.0, 0.1, 0.0, 1.0 };
+    Stage exit {"Exit", 2.0, 0.1, 1.0, 0.0 };
+    Stage cont {"Continue", 2.0, 0.1, 0.0, 0.0 };
+    
+    Asset a1 { "Shmoogle", 5e6 };
+    Asset a2 { "Shmopbox", 5e6 };
+    Asset a3 { "Shamazon", 5e6 };
+    EXPECT_EQ(death.traverse_stage(a1), Stage::EXIT);
+    EXPECT_EQ(exit.traverse_stage(a2), Stage::DIE);
+    EXPECT_EQ(cont.traverse_stage(a3), Stage::CONTINUE);
+    EXPECT_EQ(a3.value(), 12e6);
+}
